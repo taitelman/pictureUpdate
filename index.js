dddbeee5fe87c92c8aa8b5fc0d49a5003d631c30
taimor <taimor@il.ibm.com>
2020-04-29 22:16:37 +0300

    fix login sequence

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /*
  steps to run this code:
  1. create a project folder (any folder) and go to that folder. create this file as index.js
@@ -7,145 +6,170 @@
  4. node index.jsrequire('request').debug = true
  */
 
-const request = require('request').defaults({ jar: true, debug: true });
-const FormData = require('form-data');
-const cheerio = require('cheerio');
+const request = require('request').defaults({jar: true});
+const http = require('follow-redirects').http;
+const https = require('follow-redirects').https;
+var qs = require('querystring');
+
+//defaults({jar: true, debug: true});
+//const FormData = require('form-data');
+//const JSONFormatter = require('json-form-data');
+//const FileAPI = require('file-api') , File = FileAPI.File;
+
+//const cheerio = require('cheerio');
 //require('request-debug')(request);
 //request.debug=true;
 const fs = require('fs');
 
+const WEB_URL = "https://secure.konimbo.co.il/login?goto_url=%2Fadmin%2Fitems%2F";
 const LOGIN_URL = "https://secure.konimbo.co.il/user_sessions";
+const GOTO_URL="https://secure.konimbo.co.il/login?goto_url=%2Fadmin%2Forders%3Fstatus_option_title%3D%25D7%25A8%25D7%259C%25D7%2595%25D7%2595%25D7%25A0%25D7%2598%25D7%2599%25D7%2595%25D7%25AA";
 
 const UPLOAD_PIC_URL = "https://secure.konimbo.co.il/admin/items/xxx/update_spec";
 const EDIT_PIC_URL = "https://secure.konimbo.co.il/admin/items/xxx/edit_spec";
 
+const USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36";
+const ENCODED_MIME = 'application/x-www-form-urlencoded';
+
+
 let credentials = require('./passwords.json');
 console.log(`credentials = ${JSON.stringify(credentials)}`);
-  
-  async function login(callback) {
-  const options = {
-    url: LOGIN_URL,
-    method: 'POST',
-    data: {
-      username: credentials.user,
-      password: credentials.password,
-      commit: "כניסה"
-    },
-    headers: {
-      'User-Agent': 'request'
-    },
-    jar: true // for cookies
-  };
 
-  request(options , (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      console.log("logged in");
-      if (callback) {
-        callback;
-      }
+async function login(callback) {
+	const options = {
+		url: LOGIN_URL,
+		method: 'POST',
+		hostname: 'secure.konimbo.co.il',
+		//path: '/user_sessions',
+		formData: {
+			"user_session[username]": credentials.user,
+			"user_session[password]": credentials.password,
+			goto_url: "/admin/orders?status_option_title=%D7%A8%D7%9C%D7%95%D7%95%D7%A0%D7%98%D7%99%D7%95%D7%AA",
+			current_url: GOTO_URL,
+			current_ip: "10.0.1.171",
+			commit: "כניסה"
+		},
+		headers: {
+			"Content-Type" : ENCODED_MIME
+		},
+		followAllRedirects: true,
+		maxRedirects: 20,
+		jar: true // for cookies
+	};
+	
+	request(options, (error, response, body) => {
+		if (!error && response.statusCode === 200) {
+			console.log("logged in");
+			if (callback) {
+				const cookies = response.headers["set-cookie"];
+				console.log(`cookies=${JSON.stringify(cookies)}`);
+				callback(cookies);
+				return cookies;
+			}
 
-    } else {
-      console.log(`logging failed: ${response.statusCode}`);
-    }
-  });
+		} else {
+			console.log(`logging failed: ${response.statusCode}`);
+		}
+	});
 }
 
-async function uploadFile(itemNumber, filename) {
-  
-  const editURL = EDIT_PIC_URL.replace('xxx', encodeURI(itemNumber));
-  
-  request.get(editURL, (error, response, body) => {
-    if (error) {
-      return console.error('get edit item failed:', error);
-    } else {
-      if (response.statusCode === 200) {
-        const $ = cheerio.load(body);
-        const tokenElement = $('input[name="authenticity_token"]');
-        const token = tokenElement.attr('value');
-        if (!token) {
-          console.error(`failed fetching authenticity_token`);
-          return;
-        }
-        sendItemUpdate(token, itemNumber,filename);
-        console.log('get item ok:', response.statusCode);
-      }
-    }
-  
-  });
-  
- 
+async function uploadFile(itemNumber, productId, filename) {
+	
+	const editURL = EDIT_PIC_URL.replace('xxx', encodeURI(itemNumber));
+	
+	//sendItemUpdate(null, itemNumber, filename);
+	
+	request.get(editURL, (error, response, body) => {
+	  if (error) {
+	    return console.error('get edit item failed:', error);
+	  } else {
+		  const cookies = response.headers["set-cookie"];
+		  //console.log(`cookies=${JSON.stringify(cookies)}`);
+
+	    if (response.statusCode === 200) {
+	      // const $ = cheerio.load(body);
+	      // const tokenElement = $('input[name="authenticity_token"]');
+	      // const token = tokenElement.attr('value');
+	      // if (!token) {
+	      //   console.error(`failed fetching authenticity_token`);
+	      //   return;
+	      // }
+	      sendItemUpdate(itemNumber, productId, filename);
+	      console.log('get item ok:', response.statusCode);
+	    }
+	  }
+
+	});
+	
+	
 }
 
 
-function sendItemUpdate(token, itemNumber, filename) {
-  const updateURL = UPLOAD_PIC_URL.replace('xxx', encodeURI(itemNumber));
-  
-  const i =1;
-  const image = fs.createReadStream(__dirname + filename);
-  const product = [{
-    'product_images_attributes': [{
-      photo: {
-        filename: '',
-        contentType: 'application/octet-stream',
-        value: image,
-        name: filename
-      },
-      image_url: '',
-      title: '',
-      position: i+1,
-      _destroy: 0,
-      id: 3156524
-    }],
-    'product_features_attributes': [
-      {
-        position: i+1,
-        _destroy: 0,
-        group_name: '' ,
-        name:'' ,
-        value: ''
-      }
-     ],
-    separator: '',
-    remove_char:'',
-    show_as_new: ''
-  }];
-  
-  //const formData = formidable({ multiples: true });
-  const formData = new FormData();
-  formData.append("_method" , "put");
-  formData.append("authenticity_token",token);
-  formData.append("item_id",itemNumber );
-  formData.append("product_id", 2521162);
-  formData.append("product",JSON.stringify(product));
-  formData.append("save", 'שמור');
-  
-  const headers = {
-    Referer: EDIT_PIC_URL.replace('xxx', encodeURI(itemNumber)),
-    DNT: "1",
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
-    Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding":  "gzip, deflate, br"
-    
-  };
-  
-  const item_url = updateURL;
-  console.log(`sending POST request to : ${item_url}`);
-  formData.submit(item_url , (err, httpResponse, body) => {
-    if (err) {
-      return console.error('upload failed:', err);
-    }
-    if (httpResponse.statusCode === 302 && httpResponse.headers.location.endsWith('edit_spec')) {
-      console.log(`Upload successful!  Server responded with: ${httpResponse.statusCode} `);
-    } else {
-      console.log('upload almost went ok:', httpResponse.statusCode);
-    }
-    
-  });
-  
+function sendItemUpdate(itemNumber,productId, filename, cookies) {
+	const updateURL = UPLOAD_PIC_URL.replace('xxx', encodeURI(itemNumber));
+	const editURL = EDIT_PIC_URL.replace('xxx', encodeURI(itemNumber));
+	
+	let formData = {
+		'_method': 'put',
+		'authenticity_token': '',
+		'item_id': '1972243',
+		'product_id': productId,
+		'product[product_images_attributes][1][photo]': {
+			'value': fs.createReadStream('/home/taimor/dev/somethingNew/dror/resources/dot.jpg'),
+			'options': {
+				'filename': 'dot.jpg',
+				'contentType': "image/jpeg"
+			}
+		},
+		'product[product_images_attributes][1][image_url]': '',
+		'product[product_images_attributes][1][title]': 'just a title',
+		'product[product_images_attributes][1][position]': '2',
+		'product[product_images_attributes][1][_destroy]': '0',
+		'save': 'שמור',
+		'product[separator]': '',
+		'product[remove_char]': '',
+		'show_as_new': '1'
+	};
+	
+	let headers =
+		{
+			'User-Agent': USERAGENT,
+			'Content-Type': ENCODED_MIME,
+			Referer: editURL,
+			Origin: 'https://secure.konimbo.co.il/',
+			Connection: "keep-alive",
+			"Accept-Encoding": "gzip, deflate, br",
+			Host: "secure.konimbo.co.il",
+			Accept: "*/*",
+			"Cache-Control": "no-cache"
+		};
+	
+	let options = {
+		method: 'POST',
+		url: updateURL,
+		headers,
+		formData,
+		followAllRedirects: true,
+		maxRedirects: 20,
+		jar: true // for cookies
+	};
+	
+	console.log(`sending POST request to : ${updateURL}`);
+	request.post(options, (err, httpResponse, body) => {
+		if (err) {
+			console.error('upload failed:', err);
+			throw new Error(err);
+		} else if (httpResponse.statusCode === 302 && httpResponse.headers.location != null ) {
+			console.log(`Upload failed . redirect to ${httpResponse.headers.location} `);
+		} else if (httpResponse.statusCode === 200) {
+			console.log('upload Success:', httpResponse.statusCode);
+		} else {
+			console.log('upload almost went ok:', httpResponse.statusCode);
+		}
+	});
+	
 }
 
-const uploadFunc = (itemNumber,filename) => {
-  uploadFile(itemNumber, filename);
-};
-
-login(uploadFunc('1972243-בלון-אוויר-אננס', '/resources/dot.jpg'));
+login((cookies) => {
+	sendItemUpdate('1972243-בלון-אוויר-אננס', "2521162", '/resources/dot.jpg', cookies);
+});
